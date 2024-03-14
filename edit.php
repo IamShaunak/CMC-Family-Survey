12db3029048d101fc837c16e1175081f86d861f9
Shaunik <105384586+IamShaunak@users.noreply.github.com>
2024-03-14 16:59:31 +0530

    User_account

---
+++ edit.php
@@ -0,0 +1,107 @@
+<?php
+$comp_model = new SharedController;
+$page_element_id = "edit-page-" . random_str();
+$current_page = $this->set_current_page_link();
+$csrf_token = Csrf::$token;
+$data = $this->view_data;
+//$rec_id = $data['__tableprimarykey'];
+$page_id = $this->route->page_id;
+$show_header = $this->show_header;
+$view_title = $this->view_title;
+$redirect_to = $this->redirect_to;
+?>
+<section class="page" id="<?php echo $page_element_id; ?>" data-page-type="edit"  data-display-type="" data-page-url="<?php print_link($current_page); ?>">
+    <?php
+    if( $show_header == true ){
+    ?>
+    <div  class="bg-light p-3 mb-3">
+        <div class="container">
+            <div class="row ">
+                <div class="col ">
+                    <h4 class="record-title"><?php print_lang('my_account'); ?></h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+    }
+    ?>
+    <div  class="">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-7 comp-grid">
+                    <?php $this :: display_page_errors(); ?>
+                    <div  class="bg-light p-3 animated fadeIn page-content">
+                        <form novalidate  id="" role="form" enctype="multipart/form-data"  class="form page-form form-horizontal needs-validation" action="<?php print_link("account/edit/$page_id/?csrf_token=$csrf_token"); ?>" method="post">
+                            <div>
+                                <div class="form-group ">
+                                    <div class="row">
+                                        <div class="col-sm-4">
+                                            <label class="control-label" for="name"><?php print_lang('name'); ?> <span class="text-danger">*</span></label>
+                                        </div>
+                                        <div class="col-sm-8">
+                                            <div class="">
+                                                <input id="ctrl-name"  value="<?php  echo $data['name']; ?>" type="text" placeholder="<?php print_lang('enter_name'); ?>"  required="" name="name"  data-url="api/json/account_name_value_exist/" data-loading-msg="<?php print_lang('checking_availability_'); ?>" data-available-msg="<?php print_lang('available'); ?>" data-unavailable-msg="<?php print_lang('not_available'); ?>" class="form-control  ctrl-check-duplicate" />
+                                                    <div class="check-status"></div> 
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group ">
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <label class="control-label" for="mobile_no"><?php print_lang('mobile_no'); ?> <span class="text-danger">*</span></label>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <div class="">
+                                                    <input id="ctrl-mobile_no"  value="<?php  echo $data['mobile_no']; ?>" type="text" placeholder="<?php print_lang('enter_mobile_no'); ?>"  required="" name="mobile_no"  class="form-control " />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group ">
+                                            <div class="row">
+                                                <div class="col-sm-4">
+                                                    <label class="control-label" for="user_role_id"><?php print_lang('user_role_id'); ?> <span class="text-danger">*</span></label>
+                                                </div>
+                                                <div class="col-sm-8">
+                                                    <div class="">
+                                                        <select required=""  id="ctrl-user_role_id" name="user_role_id"  placeholder="<?php print_lang('select_a_value_'); ?>"    class="custom-select" >
+                                                            <option value=""><?php print_lang('select_a_value_'); ?></option>
+                                                            <?php
+                                                            $rec = $data['user_role_id'];
+                                                            $user_role_id_options = $comp_model -> user_info_user_role_id_option_list();
+                                                            if(!empty($user_role_id_options)){
+                                                            foreach($user_role_id_options as $option){
+                                                            $value = (!empty($option['value']) ? $option['value'] : null);
+                                                            $label = (!empty($option['label']) ? $option['label'] : $value);
+                                                            $selected = ( $value == $rec ? 'selected' : null );
+                                                            ?>
+                                                            <option 
+                                                                <?php echo $selected; ?> value="<?php echo $value; ?>"><?php echo $label; ?>
+                                                            </option>
+                                                            <?php
+                                                            }
+                                                            }
+                                                            ?>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-ajax-status"></div>
+                                    <div class="form-group text-center">
+                                        <button class="btn btn-primary" type="submit">
+                                            <?php print_lang('update'); ?>
+                                            <i class="icon-paper-plane"></i>
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
